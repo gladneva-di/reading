4237c20261ee2fb36807bd019556b2dd465cf822
Darya Gladneva <davydova_di@mail.ru>
2024-07-05 09:07:22 +0300

    feat: added version1

--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
 import "./styles/main.scss";
 import "./swiper-bundle.min.js";
 import "./swiper-bundle.min.css";
-import {PageFlip} from 'page-flip';
-
-// const pageFlip = new PageFlip(document.getElementById('book'),
-//     {
-//         width: 150, // required parameter - base page width
-//         height: 157,  // required parameter - base page height
-//         size: "stretch",
-//         // showCover: true
-//         minWidth: 150,
-//         maxWidth: 730,
-//         minHeight: 157,
-//         maxHeight: 755,
-//         // size: "stretch",
-//         drawShadow: false,
-//         usePortrait: false,
-//     }
-// );
-
-// pageFlip.loadFromHTML(document.querySelectorAll('.my-page'));
 
 // swiper
 
@@ -60,14 +41,3 @@ var Swipes = new Swiper(".cases__swiper", {
     },
   },
 });
-
-// const pages = document.querySelectorAll(".pageNum");
-
-// pages.forEach((element) => element.addEventListener("click", () => {
-//   console.log(element.getAttribute("page"));
-//   console.log(Number(element.getAttribute("page")));
-//   pageFlip.turnToPage(Number(element.getAttribute("page")));
-// }));
-// document.addEventListener('click', () => {
-//   console.log(pageFlip.getCurrentPageIndex());
-// })
